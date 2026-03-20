067f0471f62a7d3dd01fe8b1a91204d9c95aa7dd
Thuong Tran Dinh <thuong.trandinh@hcmut.edu.vn>
2026-03-20 19:58:29 +0700

    feat: delete HALO folder

--- HALO/drl_moudle/modules/envs/actions.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-
-ActionXY = namedtuple('ActionXY', ['vx', 'vy'])
-ActionRot = namedtuple('ActionRot', ['v', 'r'])
-ActionAW = namedtuple('ActionAW', ['a', 'w'])
-ActionDiff = namedtuple('ActionDiff', ['al', 'ar'])
