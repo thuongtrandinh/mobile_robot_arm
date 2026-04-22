d52359de964eb7dec52e4432b383239736a0715b
Thuong Tran Dinh <thuong.trandinh@hcmut.edu.vn>
2026-04-22 18:23:29 +0700

    feat:

--- src/controller/scripts/rl_ocp_policy_bridge.py
+++ src/controller/scripts/rl_ocp_policy_bridge.py
@@ -9,7 +9,6 @@ from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import rclpy
-from rclpy.action import ActionClient
 from interfaces.msg import ObstacleState, WallState, PolyState, HumanState
 from interfaces.msg import JointState as InterfaceJointState
 from interfaces.srv import OcpLocalPlann
